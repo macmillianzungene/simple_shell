4955b10ecb7777decec1fa728776c769119febb8
macmillianzungene <macmillianzungene@gmail.com>
2023-11-15 13:18:10 -0800

    testing

--- main.c
+++ main.c
@@ -1,44 +1,21 @@
-#include "shell.h"
+#include "main.h"
 
-/**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
- *
- * Return: 0 on success, 1 on error
- */
-int main(int ac, char **av)
+int main(void)
 {
-	info_t info[] = { INFO_INIT };
-	int fd = 2;
+    char *buffer = NULL;
+    size_t bufsize = 0;
+    ssize_t nread;
 
-	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
-
-	if (ac == 2)
-	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
-		{
-			if (errno == EACCES)
-				exit(126);
-			if (errno == ENOENT)
-			{
-				_eputs(av[0]);
-				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
-				_eputchar('\n');
-				_eputchar(BUF_FLUSH);
-				exit(127);
-			}
-			return (EXIT_FAILURE);
-		}
-		info->readfd = fd;
-	}
-	populate_env_list(info);
-	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+    while (1)
+    {
+        printf("$ ");
+        nread = getline(&buffer, &bufsize, stdin);
+        if (nread == -1)
+        {
+            free(buffer);
+            exit(EXIT_SUCCESS);
+        }
+        printf("%s", buffer);
+    }
+    return (0);
 }
