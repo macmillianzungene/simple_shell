11b034dd7e7bd48ef6d978545307f60d0c4de35d
macmillianzungene <macmillianzungene@gmail.com>
2023-11-15 13:38:14 -0800

    testing

--- main.c
+++ main.c
@@ -2,20 +2,20 @@
 
 int main(void)
 {
-    char *buffer = NULL;
-    size_t bufsize = 0;
-    ssize_t nread;
-
-    while (1)
-    {
-        printf("$ ");
-        nread = getline(&buffer, &bufsize, stdin);
-        if (nread == -1)
-        {
-            free(buffer);
-            exit(EXIT_SUCCESS);
-        }
-        printf("%s", buffer);
-    }
-    return (0);
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t nread;
+	
+	while (1)
+	{
+		printf("$ ");
+		nread = getline(&buffer, &bufsize, stdin);
+		if (nread == -1)
+		{
+			free(buffer);
+			exit(EXIT_SUCCESS);
+		}
+		printf("%s", buffer);
+	}
+	return (0);
 }
