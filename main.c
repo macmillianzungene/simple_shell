7f59f2b6c5d3f5c1888608ec16786f2933c60519
macmillianzungene <macmillianzungene@gmail.com>
2023-11-15 14:12:52 -0800

    Testing

--- main.c
+++ main.c
@@ -10,17 +10,24 @@ int main(void)
 	char *buffer = NULL;
 	size_t bufsize = 0;
 	ssize_t nread;
+	char *args[2];
 
 	while (1)
 	{
-		printf("$ ");
+		printf("MacMillian$ ");
 		nread = getline(&buffer, &bufsize, stdin);
 		if (nread == -1)
 		{
 			free(buffer);
 			exit(EXIT_SUCCESS);
 		}
-		printf("%s", buffer);
+		buffer[strcspn(buffer, "\n")] = 0;
+		args[0] = buffer;
+		args[1] = NULL;
+		if (execve(args[0], args, NULL) == -1)
+		{
+			printf("Error: command not found\n");
+		}
 	}
 	return (0);
 }
--- main.h
+++ main.h
@@ -4,6 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 
 #endif
