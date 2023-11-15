52c4c10cb2e0c1af6fa1aa3ee9e13304ea8e2870
macmillianzungene <macmillianzungene@gmail.com>
2023-11-15 15:22:46 -0800

    Testing

--- main.c
+++ main.c
@@ -7,6 +7,25 @@
  */
 int main(void)
 {
-	printf("Hello, Macmillian!\n");
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t nread;
+
+	while (1)
+	{
+		printf("Macmillian$ ");
+		nread = getline(&buffer, &bufsize, stdin);
+		if (nread == -1)
+		{
+			free(buffer);
+			exit(EXIT_SUCCESS);
+		}
+		if (nread > 0 && buffer[nread - 1] == '\n')
+			buffer[nread - 1] = '\0';
+		if (access(buffer, F_OK) == 0)
+			system(buffer);
+		else
+			printf("Command not found: %s\n", buffer);
+	}
 	return (0);
 }
