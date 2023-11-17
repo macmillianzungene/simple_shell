7dd93c78d09492dbcc3c210473798ddb7efc0426
macmillianzungene <macmillianzungene@gmail.com>
2023-11-17 14:20:48 -0800

    Testing

--- main.c
+++ main.c
@@ -6,8 +6,22 @@
  * Return: Always 0 (Success)
  */
 
+extern char **environ; /* Declare the environment variable array */
+
+void print_environment(void)
+{
+	char **env = environ;
+
+	while (*env)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+}
+
 int main(void)
 {
+
 	char *buffer = NULL;
 	size_t bufsize = 0;
 	ssize_t nread;
@@ -27,8 +41,20 @@ int main(void)
 		args[0] = buffer;
 		args[1] = NULL;
 
-		if (execve(args[0], args, NULL) == -1)
+		if (strcmp(args[0], "exit") == 0)
 		{
+			free(buffer);
+			exit(EXIT_SUCCESS);
+		}
+		else if
+			(strcmp(args[0], "env") == 0)
+			{
+				print_environment();
+			}
+		else
+		{
+			/* Handle other commands (search in PATH, execute, etc.) */
+			/* ... */
 			printf("Error: command not found\n");
 		}
 	}
