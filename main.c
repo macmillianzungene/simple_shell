8d4b4e80cae47526f4ef903b0f7074cd15a704b8
macmillianzungene <macmillianzungene@gmail.com>
2023-11-17 22:56:28 -0800

    Testing

--- main.c
+++ main.c
@@ -5,14 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	char *buffer = NULL;
 	size_t bufsize = 0;
 	ssize_t nread;
 	char *args[2];
-	char *path = getenv("PATH");
 
 	while (1)
 	{
@@ -25,33 +23,10 @@ int main(void)
 			exit(EXIT_SUCCESS);
 		}
 		buffer[strcspn(buffer, "\n")] = 0;
-
-		if (strcmp(buffer, "exit") == 0)
-		{
-			free(buffer);
-			exit(EXIT_SUCCESS);
-		}
-		if (strcmp(buffer, "env") == 0)
-		{
-			printf("%s\n", path);
-			continue;
-		}
 		args[0] = buffer;
 		args[1] = NULL;
 
-		if (access(args[0], F_OK) != -1)
-		{
-			if (fork() == 0)
-			{
-				if (execve(args[0], args, NULL) == -1)
-				{
-					printf("Error: command not found\n");
-				}
-			} else
-			{
-				wait(NULL);
-			}
-		} else
+		if (execve(args[0], args, NULL) == -1)
 		{
 			printf("Error: command not found\n");
 		}
