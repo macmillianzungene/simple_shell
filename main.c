dacad6d79c41ddd1af3425fa5b9a9c2aab2d486e
macmillianzungene <macmillianzungene@gmail.com>
2024-03-29 02:58:07 -0700

    updated

--- main.c
+++ main.c
@@ -1,35 +1,44 @@
-#include "main.h"
+#include "shell.h"
 
 /**
- * main - Entry point
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
  *
- * Return: Always 0 (Success)
+ * Return: 0 on success, 1 on error
  */
-int main(void)
+int main(int ac, char **av)
 {
-	char *buffer = NULL;
-	size_t bufsize = 0;
-	ssize_t nread;
-	char *args[2];
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	while (1)
+	asm ("mov %1, %0\n\t"
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
+
+	if (ac == 2)
 	{
-		printf("Macmillian$ ");
-		nread = getline(&buffer, &bufsize, stdin);
-
-		if (nread == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			free(buffer);
-			exit(EXIT_SUCCESS);
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-		buffer[strcspn(buffer, "\n")] = 0;
-		args[0] = buffer;
-		args[1] = NULL;
-
-		if (execve(args[0], args, NULL) == -1)
-		{
-			printf("Error: command not found\n");
-		}
+		info->readfd = fd;
 	}
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
